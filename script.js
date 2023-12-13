356c768ee67affe44023a0aee148fab060423d7d
YSSF8 <76586183+YSSF8@users.noreply.github.com>
2023-12-13 19:43:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,14 +14,54 @@ const letterInfo = document.querySelector('.letter-info');
 const messageZone = messageInput.parentElement;
 const mainMenu = document.querySelector('.main-menu');
 const start = document.getElementById('start');
+const coinCounter = document.getElementById('coin-counter');
+const keyboard = document.querySelector('.keyboard');
 
-messageHistory.style.paddingTop = `${userMeter.closest('.meters').offsetHeight + 8}px`;
+function fixMessageHistoryPadding() {
+    messageHistory.style.paddingTop = `${userMeter.closest('.meters').offsetHeight + 8}px`;
+    requestAnimationFrame(fixMessageHistoryPadding);
+}
+
+fixMessageHistoryPadding();
+coinCounter.innerHTML = parseInt(localStorage.getItem('coins')) || 0;
 
 messageInput.addEventListener('input', () => {
     messageInput.value = messageInput.value.replace(/[^a-zA-Zé]/, '').replace(/^./, match => match.toUpperCase());
 });
 
-const MAX_SCORE = 50;
+function fixInputByRes() {
+    if (window.matchMedia('(max-width: 600px)').matches) {
+        messageInput.setAttribute('disabled', '');
+        messageZone.style.bottom = `${keyboard.offsetHeight}px`;
+
+        keyboard.querySelectorAll('.key').forEach(key => {
+            if (!key.hasAttribute('data-click-listener-added')) {
+                key.addEventListener('click', () => {
+                    if (messageInput.value == '') {
+                        if (/<.+>.+<\/.+>$/.test(key.innerHTML)) return;
+                        messageInput.value += key.innerHTML.toUpperCase();
+                    } else {
+                        if (/<.+>.+<\/.+>$/.test(key.innerHTML)) {
+                            messageInput.value = messageInput.value.slice(0, -1);
+                        } else {
+                            messageInput.value += key.innerHTML.toLowerCase();
+                        }
+                    }
+                });
+                key.setAttribute('data-click-listener-added', '');
+            }
+        });
+    } else {
+        messageInput.removeAttribute('disabled');
+        messageZone.style.bottom = '0';
+    }
+
+    requestAnimationFrame(fixInputByRes);
+}
+
+fixInputByRes();
+
+const MAX_SCORE = 70;
 let difficulty = 'medium';
 let defaultTimer = 15;
 let timer = defaultTimer;
@@ -30,8 +70,129 @@ let botScore = 0;
 let timeout = 3;
 let usedWords = [];
 let timerId;
+let coins = parseInt(localStorage.getItem('coins')) || 0;
 let chosenLetter = String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1)) + 65);
+let earnings = {
+    easy: 100,
+    medium: 500,
+    hard: 1000,
+    expert: 2000
+}
 letterInfo.innerHTML = letterInfo.innerHTML.replace('?', chosenLetter);
+
+document.querySelectorAll('.bought-items button').forEach(btn => {
+    let savedItemValue = localStorage.getItem(btn.getAttribute('data-saved'));
+
+    if (savedItemValue !== null && !isNaN(savedItemValue)) {
+        btn.innerHTML = btn.innerHTML.replace('?', savedItemValue || '0');
+
+        btn.addEventListener('click', () => {
+            let savedItem = parseInt(localStorage.getItem(btn.getAttribute('data-saved')));
+            
+            if (savedItem > 0) {
+                savedItem--;
+                localStorage.setItem(btn.getAttribute('data-saved'), savedItem);
+                btn.innerHTML = btn.innerHTML.replace(/\d+/, savedItem);
+                
+                switch (btn.getAttribute('data-saved')) {
+                    case 'hint':
+                        btn.setAttribute('disabled', '');
+                        hint(chosenLetter).then(hints => {
+                            const hintParent = document.createElement('div');
+                            hintParent.style.left = '50%';
+                            hintParent.style.transform = 'translateX(-50%)';
+                            hintParent.style.position = 'fixed';
+                            hintParent.style.width = 'max-content';
+                            document.body.appendChild(hintParent);
+
+                            for (let hint of hints) {
+                                const hintItem = document.createElement('button');
+                                hintItem.innerHTML = hint.replace(/^./, match => match.toUpperCase());
+                                hintItem.classList.add('hint');
+                                hintItem.role = 'button';
+                                hintItem.style.marginLeft = 'calc(var(--common-px) / 2.5)';
+                                hintParent.appendChild(hintItem);
+
+                                hintItem.addEventListener('click', () => {
+                                    messageInput.value = hintItem.innerHTML;
+                                    btn.removeAttribute('disabled');
+                                    hintParent.remove();
+                                    sendMessage.click();
+                                });
+                            }
+
+                            if (window.matchMedia('(max-width: 600px)').matches) {
+                                hintParent.style.bottom = `${(messageInput.getBoundingClientRect().height + (hintParent.offsetHeight / 2)) + keyboard.offsetHeight}px`;
+                            } else {
+                                hintParent.style.bottom = `${messageInput.getBoundingClientRect().height + (hintParent.offsetHeight / 2)}px`;
+                            }
+                        });
+                        break;
+                    case 'halfPoints':
+                        pointManager('/', 'bot');
+                        break;
+                    case 'doublePoints':
+                        pointManager('*', 'player');
+                        break;
+                    case 'changeLetter':
+                        changeLetter(String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1)) + 65));
+                        break;
+                    default:
+                        sendNewMessage('An error occurred: Couldn\'t operate the item', true);
+                }
+            }
+
+            async function hint(beginning = '') {
+                try {
+                    let response = await fetch(`https://random-word-api.herokuapp.com/word?number=100&length=${Math.floor(Math.random() * (10 - 7 + 1)) + 7}`);
+                    if (!response.ok) {
+                        sendNewMessage(`Error fetching: ${response.status}`);
+                        return;
+                    }
+                    clearInterval(timerId);
+                    let words = await response.json();
+                    let filteredWords = words.filter(word => word.toUpperCase().startsWith(beginning.toUpperCase()));
+                    if (filteredWords.length >= 3) {
+                        let promises = filteredWords.slice(0, 3).map(word => fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`));
+                        let responses = await Promise.all(promises);
+                        let hints = responses.filter(response => response.ok).map((response, index) => filteredWords[index]);
+                        if (hints.length >= 3) {
+                            startTimer();
+                            return hints;
+                        } else {
+                            return hint(beginning);
+                        }
+                    } else {
+                        return hint(beginning);
+                    }
+                } catch (error) {
+                    sendNewMessage(`An error occurred: ${error.message}`, true);
+                }
+            }
+
+            function pointManager(operator, agent) {
+                if (operator == '/') {
+                    botScore = Math.floor(botScore / 2);
+                    botMeterValue.innerHTML = botScore;
+                    botMeterPercentage.style.width = `${(botScore / MAX_SCORE) * 100}%`;
+                } else if (operator == '*') {
+                    playerScore = playerScore * 2;
+                    userMeterValue.innerHTML = playerScore;
+                    userMeterPercentage.style.width = `${(playerScore / MAX_SCORE) * 100}%`;
+                    playerScore >= MAX_SCORE ? winningScene('plater') : undefined;
+                }
+            }
+
+            function changeLetter(letter) {
+                chosenLetter = letter;
+                letterInfo.innerHTML = letterInfo.innerHTML.replace(/:\s.+/, `: ${chosenLetter}`);
+            }
+        });
+    } else {
+        localStorage.setItem(btn.getAttribute('data-saved'), '0');
+        btn.innerHTML = btn.innerHTML.replace('?', '0');
+    }
+});
 
 sendMessage.addEventListener('click', () => {
     let rotationLevel = 0;
@@ -206,7 +367,6 @@ function findWordStartingWith(letter) {
 function fixScrolling() {
     if (document.body.scrollHeight + 10 > document.body.clientHeight) {
         messageZone.style.position = 'fixed';
-        messageZone.style.bottom = '0';
         messageHistory.style.marginBottom = 'calc(var(--common-px) * 3)';
 
         messageHistory.querySelectorAll('.sent-message').forEach(message => {
@@ -214,7 +374,11 @@ function fixScrolling() {
         });
 
         if (messageHistory.lastElementChild) {
-            messageHistory.lastElementChild.style.marginBottom = '1in';
+            if (window.matchMedia('(max-width: 600px)').matches) {
+                messageHistory.lastElementChild.style.marginBottom = `${keyboard.offsetHeight + messageZone.offsetHeight + 27}px`;
+            } else {
+                messageHistory.lastElementChild.style.marginBottom = '1in';
+            }
         }
     }
 
@@ -251,6 +415,7 @@ function winningScene(agent = '') {
     const won = document.createElement('div');
     won.innerHTML = `
     <h2>${agent == 'player' ? 'You' : 'The bot has'} won!</h2>
+    ${agent == 'player' ? `<div>Reward: +${difficulty == 'easy' ? earnings.easy : (difficulty == 'hard' ? earnings.hard : (difficulty == 'expert' ? earnings.expert : earnings.medium))} coins</div>` : ''}
     <div ${agent == 'player' ? 'style="margin-bottom: var(--common-px);"' : ''}>Winner's score: ${agent == 'player' ? playerScore : botScore}</div>
     ${agent == 'bot' ? `<div style="margin-bottom: var(--common-px);">Your score: ${playerScore}</div>` : ''}
     <div class="scene-buttons">
@@ -260,6 +425,30 @@ function winningScene(agent = '') {
     `;
     won.classList.add('scene');
     document.body.appendChild(won);
+
+    if (agent == 'player') {
+        switch (difficulty) {
+            case 'easy':
+                coins += earnings.easy;
+                localStorage.setItem('coins', coins);
+                coinCounter.innerHTML = coins;
+                break;
+            case 'hard':
+                coins += earnings.hard;
+                localStorage.setItem('coins', coins);
+                coinCounter.innerHTML = coins;
+                break;
+            case 'expert':
+                coins += earnings.expert;
+                localStorage.setItem('coins', coins);
+                coinCounter.innerHTML = coins;
+                break;
+            default:
+                coins += earnings.medium;
+                localStorage.setItem('coins', coins);
+                coinCounter.innerHTML = coins;
+        }
+    }
 
     won.querySelector('.replay').addEventListener('click', () => {
         localStorage.setItem('replay', true);
@@ -296,6 +485,8 @@ start.addEventListener('click', () => {
             messageInput.focus();
             currentDifficulty.innerHTML = currentDifficulty.innerHTML.replace('Unselected', btn.getAttribute('data-difficulty').replace(/^./, match => match.toUpperCase()));
             difficultySelection.remove();
+
+            document.querySelectorAll('.bought-items button').forEach(btn => btn.removeAttribute('disabled'));
         });
     });
 });
@@ -306,9 +497,104 @@ if (localStorage.getItem('replay')) {
     messageInput.focus();
     difficulty = localStorage.getItem('difficulty');
     currentDifficulty.innerHTML = currentDifficulty.innerHTML.replace('Unselected', difficulty.replace(/^./, match => match.toUpperCase()));
+    document.querySelectorAll('.bought-items button').forEach(btn => btn.removeAttribute('disabled'));
     localStorage.removeItem('difficulty');
     localStorage.removeItem('replay');
 }
+
+let openedStores = 0;
+
+coinCounter.closest('.coins').addEventListener('click', () => {
+    openedStores++;
+    if (openedStores > 1) return;
+
+    const store = document.createElement('div');
+    store.classList.add('store');
+    document.body.appendChild(store);
+
+    store.innerHTML = `
+    <div class="header">
+        <div class="title">
+            <img src="./assets/store.png" height="25" draggable="false" alt="">
+            <div>Store</div>
+        </div>
+        <div class="close">
+            <span class="material-symbols-outlined">close</span>
+        </div>
+    </div>
+    <div class="content"></div>
+    `;
+
+    const content = store.querySelector('.content');
+
+    fetch('./items.json')
+        .then(res => res.json())
+        .then(items => {
+            let itemHTML = '';
+            for (let item of items) {
+                itemHTML += `
+                <div class="item" title="${item.description}" data-price="${item.price}" data-bought="${eval(item.bought)}" id="${item.name.toLowerCase().replace(' ', '-')}">
+                    <img src="${item.image}" height="120" draggable="false" alt="">
+                    <div>${item.name}</div>
+                    <sup style="color: #d9d9d9;">$${item.price}</sup>
+                </div>
+                `;
+            }
+            content.innerHTML = itemHTML;
+
+            content.querySelectorAll('.item').forEach(item => {
+                item.addEventListener('click', () => {
+                    if (coins < parseInt(item.getAttribute('data-price'))) {
+                        item.style.backgroundColor = '#8d2d2d';
+                        setTimeout(() => {
+                            item.style.removeProperty('background-color');
+                        }, 500);
+                        return;
+                    }
+
+                    coins -= item.getAttribute('data-price');
+                    localStorage.setItem('coins', coins);
+                    coinCounter.innerHTML = coins;
+
+                    switch (item.id) {
+                        case 'hint':
+                            let hintCount = parseInt(localStorage.getItem('hint') || '0');
+                            hintCount++;
+                            localStorage.setItem('hint', hintCount.toString());
+                            break;
+                        case 'half-points':
+                            let halfPointsCount = parseInt(localStorage.getItem('halfPoints') || '0');
+                            halfPointsCount++;
+                            localStorage.setItem('halfPoints', halfPointsCount.toString());
+                            break;
+                        case 'double-points':
+                            let doublePointsCount = parseInt(localStorage.getItem('doublePoints') || '0');
+                            doublePointsCount++;
+                            localStorage.setItem('doublePoints', doublePointsCount.toString());
+                            break;
+                        case 'change-letter':
+                            let changeLetterCount = parseInt(localStorage.getItem('changeLetter') || '0');
+                            changeLetterCount++;
+                            localStorage.setItem('changeLetter', changeLetterCount.toString());
+                            break;
+                        default:
+                            alert('An error occurred: Purchase is not successful');
+                    }
+
+                    item.style.backgroundColor = '#28591d';
+                    setTimeout(() => {
+                        item.style.removeProperty('background-color');
+                    }, 500);
+                });
+            });
+        })
+        .catch(error => alert('An error occurred: ' + error.message));
+
+    store.querySelector('.close').addEventListener('click', () => {
+        store.remove();
+        openedStores = 0;
+    });
+});
 
 function startTimer() {
     timerId = setInterval(() => {
